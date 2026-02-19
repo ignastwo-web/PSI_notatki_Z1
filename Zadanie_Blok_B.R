d097677c5200882a2b8d3b142ea94203c0c1a801
Ignacy Tworzydło <ignastwo@gmail.com>
2026-02-19 18:02:45 +0100

    Add files via upload

---
+++ Zadanie_Blok_B.R
@@ -0,0 +1,26 @@
+# Zadanie 4: Kalkulator
+kalkulator = function(a, b, operacja){
+  
+  if(operacja == '*') {
+    return(a * b)
+  } else if(operacja == '+') {
+    return(a + b)
+  } else if(operacja == '-') {
+    return(a - b)
+  } else if(operacja == '/') {
+    if(b == 0) return("B³¹d: dzielenie przez zero!")
+    return(a / b)
+  } else {
+    return("Nieznana operacja")
+  }
+}
+
+# Testy kalkulatora
+print(kalkulator(20, 2, "+")) 
+print(kalkulator(20, 2, "-"))
+print(kalkulator(20, 2, "*"))
+print(kalkulator(20, 2, "/"))
+
+print(kalkulator(15, 0, "/"))
+print(kalkulator(15, 0, "+")) 
+print(kalkulator(15, 0, "%")) 
---
+++ Zadanie_blok_A.R
@@ -0,0 +1,17 @@
+# Zadanie 2 Przelicznik walut
+przelicz_walute = function(kwota, kurs = 4.32) {
+  return(kwota * kurs)
+}
+
+
+print(przelicz_walute(100)) 
+
+
+przelicz_walute = function(kwota, kurs = 4.5) {
+  return(kwota * kurs)
+}
+
+
+print(przelicz_walute(100)) 
+
+
